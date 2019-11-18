b802e1a5a0074296946caa9a335023d51ff65fef
Anderson Alves de Almeida <contato@andersonalves.com>
2019-11-18 16:58:38 -0300

    Adicionar protocolo na url do proxy

--- InstagramAPI/InstagramAPI.py
+++ InstagramAPI/InstagramAPI.py
@@ -85,7 +85,7 @@ class InstagramAPI:
 
         if proxy is not None:
             print('Set proxy!')
-            proxies = {'http': proxy, 'https': proxy}
+            proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy}
             self.s.proxies.update(proxies)
 
     def set_session(self, **kwargs):
